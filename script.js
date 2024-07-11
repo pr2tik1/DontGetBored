e42398afd013532e50789a124944c19eb67a4738
Pratik Kumar <pr2tik1@gmail.com>
2024-07-11 07:57:06 +0000

    push v1

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('get-a-task-button');
+    const taskDisplay = document.getElementById('task-display');
+
+    button.addEventListener('click', () => {
+        fetch('https://bored-api.appbrewery.com/random')
+            .then(response => response.json())
+            .then(data => {
+                taskDisplay.textContent = `${data.activity}`;
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                taskDisplay.textContent = 'Please wait for few seconds and then retry !';
+            });
+    });
+});
